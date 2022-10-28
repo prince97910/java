a5e894859a18537a1d31f59e8ae5461ffafe1ce7
prince97910 <113010157+prince97910@users.noreply.github.com>
2022-10-28 12:17:21 +0530

    Update FileWriterExample.java

--- lab/FileWriterExample.java
+++ lab/FileWriterExample.java
@@ -4,7 +4,7 @@ public class FileWriterExample {
     public static void main(String args[]){    
          try{    
            FileWriter fw=new FileWriter("D:\\testout.txt");    
-           fw.write("Welcome to javaTpoint.");    
+           fw.write("Welcome to home.");    
            fw.close();    
           }catch(Exception e){System.out.println(e);}    
           System.out.println("Success...");    
